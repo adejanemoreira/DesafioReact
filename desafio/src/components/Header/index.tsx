d3c67959db29a43b19e1af33b724c570ef1fd28d
Adejane Moreira <adjanead1@gmail.com>
2022-11-03 00:29:01 -0300

    Add files via upload

---
+++ desafio/src/components/Header/index.tsx
@@ -0,0 +1,37 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import Image from 'next/future/image'
+import Link from 'next/link'
+import { useCallback } from 'react'
+import { useShoppingCart } from 'use-shopping-cart'
+
+import ButtonCart from '@components/ButtonCart'
+
+import logoImg from "../../assets/logo.svg"
+import { HeaderContainer } from './styles'
+
+const Header = () => {
+  const { cartCount, handleCartClick } = useShoppingCart()
+
+  const handleToggle = useCallback(() => {
+    console.log('here', cartCount)
+    handleCartClick()
+  }, [])
+
+  return (
+    <HeaderContainer>
+      <Link href="/">
+        <a>
+          <Image src={logoImg} alt="Teste" />
+        </a>
+      </Link>
+      <ButtonCart 
+        color='gray' 
+        badge={cartCount ? cartCount : undefined} 
+        onClick={handleToggle} 
+        disabled={cartCount && cartCount > 0 ? false : true}
+      />
+    </HeaderContainer>
+  )
+}
+
+export { Header }
